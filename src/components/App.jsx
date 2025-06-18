ff8f3362b3cf8f495a632358c8c9e8a0ed18a4dc
Vasundhara <parakhvasundhara@gmail.com>
2025-06-18 20:26:40 +0200

    fixed desctructive sentences, changed greetings, make sure it says it's just a bot

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -83,8 +83,8 @@ const App = ({ defaultStsConfig }) => {
     }
   })();
   const greeting = selectedBook
-    ? `Hello ${userName}! I'm Miotomo! Your happy book buddy! Are you enjoying "${selectedBook.title}"?`
-    : `Hello ${userName}! I'm Miotomo! Your happy book buddy! Are you enjoying your book?`;
+    ? `Hello ${userName}! I'm Miotomo! Are you enjoying "${selectedBook.title}"?`
+    : `Hello ${userName}! I'm Miotomo! Are you enjoying your book?`;
 
   // 6. useMemo hooks (after the values they depend on are defined)
   const updatedStsConfig = useMemo(
