8b8e7aa2a3b842c690064b984ce2e3899aaaa91b
Carlo <littleark@gmail.com>
2025-09-02 19:28:30 +0200

    splitting chunks, and made sure that the icons are at the right place

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,20 @@ export default defineConfig(({ mode }) => {
     },
     base: "/",
     build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              // Split vendor code into separate chunks
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          },
+        },
+      },
       sourcemap: true,
     },
   };
