aef90bbb82ac41c82fe993bd053c31d0be693f12
Carlo <littleark@gmail.com>
2026-01-23 19:38:43 +0100

    First version of the spelling game inside the parents section

---
+++ src/components/sections/RewardsSection.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import VisualSpellingGame from "../features/spelling/VisualSpellingGame";
+
+const RewardsSection: React.FC = () => {
+  return <VisualSpellingGame />;
+};
+
+export default RewardsSection;
